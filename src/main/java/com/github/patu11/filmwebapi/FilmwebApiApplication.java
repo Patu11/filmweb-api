ae07dd2eabbecae1c64123a9c19c3772201749e2
Patryk Warchoł <patryk.t.warchol@gmail.com>
2022-09-10 21:43:57 +0200

    Initial commit

---
+++ src/main/java/com/github/patu11/filmwebapi/FilmwebApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.patu11.filmwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmwebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmwebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/patu11/filmwebapi/FilmwebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.patu11.filmwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmwebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
