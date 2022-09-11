a90bd1930bbf7d8712de24784c7d06d617dd442f
Patryk Warchoł <patryk.t.warchol@gmail.com>
2022-09-11 15:53:40 +0200

    Add docker compose support

--- src/main/java/com/github/patu11/filmwebapi/api/config/RedisConfig.java
+++ src/main/java/com/github/patu11/filmwebapi/api/config/RedisConfig.java
@@ -1,16 +1,23 @@
 package com.github.patu11.filmwebapi.api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
+	@Value("${spring.redis.host}")
+	private String REDIS_HOST;
+	@Value("${spring.redis.port}")
+	private Integer REDIS_PORT;
 
 	@Bean
 	JedisConnectionFactory jedisConnectionFactory() {
-		return new JedisConnectionFactory();
+		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(REDIS_HOST, REDIS_PORT);
+		return new JedisConnectionFactory(config);
 	}
 
 	@Bean
