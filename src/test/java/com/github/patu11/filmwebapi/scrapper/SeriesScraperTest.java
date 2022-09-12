109927a3702de300b6fa91f9a54d0ea96049c201
Patryk Warchoł <patryk.t.warchol@gmail.com>
2022-09-12 19:22:08 +0200

    Change action

---
+++ src/test/java/com/github/patu11/filmwebapi/scrapper/SeriesScraperTest.java
@@ -0,0 +1,13 @@
+package com.github.patu11.filmwebapi.scrapper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SeriesScraperTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+}
