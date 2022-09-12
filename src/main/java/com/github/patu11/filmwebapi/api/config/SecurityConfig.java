a644a96b7d25211b8f7da328a3911b21338a0ffc
Patryk Warchoł <patryk.t.warchol@gmail.com>
2022-09-12 17:41:13 +0200

    Add support for Spring Actuator

---
+++ src/main/java/com/github/patu11/filmwebapi/api/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.github.patu11.filmwebapi.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+				.csrf().disable()
+				.authorizeRequests()
+				.antMatchers("/").permitAll()
+				.antMatchers(HttpMethod.POST, "/api/v1/**").permitAll()
+				.antMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
+				.anyRequest()
+				.authenticated()
+				.and()
+				.formLogin()
+				.and()
+				.httpBasic();
+
+		return http.build();
+	}
+}
